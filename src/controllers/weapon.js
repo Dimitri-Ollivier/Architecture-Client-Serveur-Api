f4973ca5bd61d69c008fd72fc73c2d20ad54e4f0
dimit <dimitri.ollivier14@gmail.com>
2022-05-19 23:25:45 +0200

    init

---
+++ src/controllers/weapon.js
@@ -0,0 +1,70 @@
+const pool = require("../../db");
+const queries = require('../queries/weapon');
+
+const getPictures = (req, res) => {
+    pool.query(queries.getPictures, (error, results) => {
+        if (error) throw error;
+
+        res.status(200).json(results.rows);
+    });
+};
+
+const getPictureById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query(queries.getPictureById, [id], (error, results) => {
+        if (error) throw error;
+
+        res.status(200).json(results.rows);
+    });
+};
+
+const addPicture = (req, res) => {
+    const { name, picture } = req.body;
+
+    pool.query(queries.addPicture, [name, picture], (error, results) => {
+        if (error) throw error;
+        res.status(201).send("Student Created Successfully!");
+    });
+};
+
+const updatePicture = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+    pool.query(queries.getPictureById, [id], (error, results) => {
+        if (error) throw error;
+        const noPicturesFound = !results.rows.length;
+        if (noPicturesFound) {
+            res.send("Pictures does not exist in the database.");
+        }
+
+        pool.query(queries.updatePicture, [name, id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send("Student Deleted Successfully!");
+        });
+    });
+};
+
+const deletePicture = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getPictureById, [id], (error, results) => {
+        if (error) throw error;
+        const noPicturesFound = !results.rows.length;
+        if (noPicturesFound) {
+            res.send("Pictures does not exist in the database.");
+        }
+
+        pool.query(queries.deletePicture, [id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send("Student Deleted Successfully!");
+        });
+    });
+};
+
+module.exports = {
+    getPictures,
+    getPictureById,
+    addPicture,
+    updatePicture,
+    deletePicture,
+};
---
+++ src/routes/weapon.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const controller = require('../controllers/weapon');
+
+const router = Router();
+
+router.get("/", controller.getPictures);
+router.get("/:id", controller.getPictureById);
+router.put("/", controller.addPicture);
+router.post("/:id", controller.updatePicture);
+router.delete("/:id", controller.deletePicture);
+
+module.exports = router;
