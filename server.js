3e3cb140ef3261a0a95c4e2b1895cf8c062a3a28
dimit <dimitri.ollivier14@gmail.com>
2022-07-22 02:42:01 +0200

    acces CORS

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ mongoose.connect(mongoUri, {useUnifiedTopology: true, useNewUrlParser: true})
         console.log("connected to mongoDB")
     }).catch(err => console.log(err.message));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use(express.json());
 
 app.use("/weapons", weaponRoutes);
